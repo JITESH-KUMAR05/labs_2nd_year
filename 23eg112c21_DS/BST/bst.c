99718e06e32a823134ca26ec0f578ed6c20f792f
jitesh <23eg112c21@anurag.edu.in>
2024-07-30 15:56:45 +0530

    binary search tree

---
+++ 23eg112c21_DS/BST/bst.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+struct node *create();
+void insert(struct node * , struct node *);
+void inorder(struct node *);
+int main()
+{
+    char ch;
+    struct node *root = NULL , *newnode;
+
+    do{
+        newnode = create();
+        if(root == NULL)
+        root = newnode;
+        else
+        insert(root , newnode);
+        printf("Do you want to insert (y/n) ? ");
+        getchar();
+        scanf("%c",&ch);
+    }while(ch =='y' || ch == 'Y');
+    printf("\nInorder Traversal\n");
+    inorder(root);
+    return 0;
+}
+
+struct node *create(){
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    int data;
+    printf("Enter the data : ");
+    scanf("%d",&data);
+    newnode->left = newnode->right = NULL;
+    newnode->data = data;
+    return newnode;
+}
+
+void insert(struct node *root , struct node * newnode){
+    if(newnode->data < root->data){
+        if(root->left != NULL)
+        insert(root->left,newnode);
+        else
+        root->left = newnode;
+    }
+    if(newnode->data > root->data){
+        if(root->right != NULL)
+        insert(root->right ,newnode);
+        else
+        root->right = newnode;
+
+    }
+}
+
+void inorder(struct node *root){
+    if(root != NULL){
+        inorder(root->left);
+        printf("%d\t",root->data);
+        inorder(root->right);
+    }
+}
