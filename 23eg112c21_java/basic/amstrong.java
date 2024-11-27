ab4827dae132590b0f08d3970d90ee5002bc7896
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2024-11-27 07:04:21 +0000

    palindrome completed

--- 23eg112c21_java/basic/amstrong.java
+++ 23eg112c21_java/basic/amstrong.java
@@ -1,19 +1,22 @@
 public class amstrong {
     public static void main(String[] args) {
-        int n = 371;
+        // int n = 371;
+        int n = 54748;
         int tmp = n;
         int ams = 0;
         while (tmp > 0) {
             int digit = tmp % 10;
-            ams = (ams ) + (digit * digit * digit);
+            ams = (ams ) + (digit * digit * digit * digit * digit);
             tmp = tmp / 10;
             
         }
         if(ams == n){
             System.out.println("amstrong");
+            System.out.println(ams + " " + n);
         }
         else{
             System.out.println("Not amstrong");
+            System.out.println(ams + " " + n);
         }
     }
 }
