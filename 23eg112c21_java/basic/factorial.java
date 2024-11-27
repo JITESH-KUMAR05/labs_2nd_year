fd4a76af9d9ea19fd232c11a56a8d21866288feb
AU wala jitesh <ausehain@gmail.com>
2024-11-27 05:53:58 +0530

    java lab started

--- 23eg112c21_java/basic/factorial.java
+++ 23eg112c21_java/basic/factorial.java
@@ -1,4 +1,13 @@
 public class factorial {
+    static public void main(String args[]){
+        int n = 3;
+        int f = 1;
+        for(int i=1;i<=n;i++){
+            f = f * i;
+
+        }
+        System.err.println("Factorial of "+n+" is "+f);
+    }
 
     
 }
