d4e7d918e7d6ddca33c4f4fb46e37560174ba0be
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2024-12-20 08:51:29 +0000

    sconds

--- 23eg112c21_py/basic.py/tcs_problem.py
+++ 23eg112c21_py/basic.py/tcs_problem.py
@@ -3,5 +3,15 @@
 # salary = (bPay + (bPay*0.8)+(bPay*0.3-(bPay*0.12)))
 
 # print("Your salary is: ", salary)
-for i in range(0,-2,-1):
-    print(i)
+# for i in range(0,-2,-1):
+    # print(i)
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    remaining_seconds = seconds % 60
+    return f"{hours}:{minutes}:{remaining_seconds}"
+
+# Example usage
+input_seconds = 25300
+output = convert_seconds(input_seconds)
+print(output)  # Output: 7:1:40
