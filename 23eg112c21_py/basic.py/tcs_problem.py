69188d8f7d80ba87b1d6fb994eab4abdf665a998
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2024-12-20 08:36:04 +0000

    instacs

--- 23eg112c21_py/basic.py/tcs_problem.py
+++ 23eg112c21_py/basic.py/tcs_problem.py
@@ -1,5 +1,7 @@
-bPay = int(input("Enter salary or basic pay: "))
+# bPay = int(input("Enter salary or basic pay: "))
 
-salary = (bPay + (bPay*0.8)+(bPay*0.3-(bPay*0.12)))
+# salary = (bPay + (bPay*0.8)+(bPay*0.3-(bPay*0.12)))
 
-print("Your salary is: ", salary)
+# print("Your salary is: ", salary)
+for i in range(0,-2,-1):
+    print(i)
